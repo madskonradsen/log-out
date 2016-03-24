3dd50dab023efa893e5c49b811137d052255d97c
Mads Konradsen <madskonradsen@gmail.com>
2016-03-24 22:07:05 +0100

    Removed dateformat as a dependency as it required 30+ dependencies...

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var colors = require('colors');
-var dateformat = require('dateformat');
 
 function timestamp() {
-	return dateformat(new Date(), "dd/mm hh:MM:ss")
+    var date = new Date();
+
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    month = (month < 10 ? "0" : "") + month;
+    day = (day < 10 ? "0" : "") + day;
+    hour = (hour < 10 ? "0" : "") + hour;
+    min = (min < 10 ? "0" : "") + min;
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var str = day + "/"+ month + "/" + date.getFullYear() + " " +  hour + ":" + min + ":" + sec;
+
+    return str;
 }
 
 exports.warn = function (msg) {
